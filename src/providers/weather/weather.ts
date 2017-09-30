e4308a340f18e8e18585d1d12cb84805e2513309
prabhjyotgambhir <prabhjyotgambhir1994@gmail.com>
2017-10-01 03:11:56 +0530

    api-usage

--- src/providers/weather/weather.ts
+++ src/providers/weather/weather.ts
@@ -2,17 +2,19 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the WeatherProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class WeatherProvider {
+	apiKey = 'b3939763dea893d5';
+	url;
 
   constructor(public http: Http) {
+  	this.url = 'http://api.wunderground.com/api/'+ this.apiKey +'/conditions/q';
     console.log('Hello WeatherProvider Provider');
+  }
+
+  getWeatherUpdate(city,state) {
+  	return this.http.get(this.url + '/' + state + '/' + city + '.json')
+  		.map(res => res.json());
   }
 
 }
